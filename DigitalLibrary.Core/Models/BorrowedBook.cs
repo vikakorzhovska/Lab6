ab1442d08eeceb93f2cc1a1eb751797d3eb54431
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-05-14 18:00:23 +0300

    added BorrowedBook model for accounting for borrowed books

---
+++ DigitalLibrary.Core/Models/BorrowedBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalLibrary.Core.Models
+{
+    public class BorrowedBook
+    {
+        public int Id { get; set; }
+
+        public string BorrowerName { get; set; }
+        public DateTime BorrowedAt { get; set; }
+        public DateTime? ReturnedAt { get; set; }
+
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
