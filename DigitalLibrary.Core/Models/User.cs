28cc0fb2c0af0e7cd1f1ac838e287e4337a65e34
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-05-14 18:06:22 +0300

    added User model to store user data

---
+++ DigitalLibrary.Core/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalLibrary.Core.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        public ICollection<BorrowedBook> BorrowedBooks { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
