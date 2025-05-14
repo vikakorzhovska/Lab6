e2554a524b8a31a96178cdd9cdd717013c023b7e
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-05-14 17:49:39 +0300

    added Publisher model to store publisher information

---
+++ DigitalLibrary.Core/Models/Publisher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalLibrary.Core.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Website { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
