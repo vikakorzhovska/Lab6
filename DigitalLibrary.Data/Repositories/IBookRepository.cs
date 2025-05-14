748db62e6c9802304a3fa9dd7e4a6f2b93d3031d
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-05-14 19:17:14 +0300

    added IBookRepository and BookRepository implementation

---
+++ DigitalLibrary.Data/Repositories/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using DigitalLibrary.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalLibrary.Data.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book?> GetByIdAsync(int id);
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(int id);
+    }
+}
