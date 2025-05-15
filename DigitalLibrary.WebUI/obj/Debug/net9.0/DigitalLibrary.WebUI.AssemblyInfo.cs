c7d772f395a824e2a19550a1d10ea3d2bfadc859
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-05-15 17:24:32 +0300

    added Razor Views to view users and their roles

--- DigitalLibrary.WebUI/obj/Debug/net9.0/DigitalLibrary.WebUI.AssemblyInfo.cs
+++ DigitalLibrary.WebUI/obj/Debug/net9.0/DigitalLibrary.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalLibrary.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c40bd3b5552da69889e51432324586d9e47690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2672c21e5d0780b55a242e41787ab67482b4b45e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalLibrary.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalLibrary.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
